264696bd789f0c80e37a48e0fe90a2efbd6d8f2b
Parker <parkermatheson@gmail.com>
2018-03-28 12:06:01 -0600

    Angular app component
    This commit updates the typescript and javascript files for the app component

--- freelance-camp-fe/app/app.component.ts
+++ freelance-camp-fe/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'app',
-  template: `
-
-  <h1>Navigation</h1>
-
-  <router-outlet></router-outlet>
-  `
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent{
   title: 'Freelance Bootcamp Dashboard';
